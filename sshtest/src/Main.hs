22d6705ca2d3f0735ee92eac2e671c26d83404c7
Tomas Stenlund <tomas.stenlund@permobil.com>
2017-07-14 12:51:09 +0200

    Added sshtest

--- sshtest/src/Main.hs
+++ sshtest/src/Main.hs
@@ -5,7 +5,7 @@ import Network.SSH.Client.SimpleSSH
 main :: IO ()
 main = do
   val <- runSimpleSSH $
-    withSessionPassword "127.0.0.1" 22 "/home/tomas/.ssh/known_hosts" "tomas" "#C0lgate!#" $ flip execCommand "ls"
+    withSessionPassword "127.0.0.1" 22 "/home/tomas/.ssh/known_hosts" "tomas" "password" $ flip execCommand "ls"
   putStrLn $ show val
   
   
